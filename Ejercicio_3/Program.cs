9f1a177d889dc26c965cffffb3156b0af0c4bff5
JOAQUÍN IGNACIO VEGA RIVERA <j.vega49@alumnos.santotomas.cl>
2021-04-15 15:55:29 -0400

    Proyecto inicial

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dis = 0;
+            Console.WriteLine("Inserte la distancia recorrida");
+            dis = double.Parse(Console.ReadLine());
+            double vel = dis / 6;
+            Console.WriteLine("La velocidad será de: " + vel);
+            if (vel > 100)
+            {
+                Console.WriteLine("El vehiculo se está desplazando a mayor velocidad");
+            }
+            else
+            {
+                Console.WriteLine("El vehiculo se está desplazando a una velocidad moderada");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            double notas = 0;
+            double prom = 0;
+            Console.WriteLine("Ingrese la cantidad de notas");
+            int cantidad = int.Parse(Console.ReadLine());
+            while ( contador < cantidad)
+            {
+                Console.WriteLine("Ingrese una nota");
+                notas = double.Parse(Console.ReadLine());
+                contador++;
+                int a = 0;
+                while ( a == 0)
+                {
+                    if (notas <= 1 && notas <= 7)
+                    {
+                        a = 1;
+                    }
+                    else
+                    {
+                        a = 0;
+                        Console.WriteLine("Ingrese una nota valida");
+                        notas = double.Parse(Console.ReadLine());
+                    }
+                }
+                prom += notas;
+            }
+            prom /= cantidad;
+            Console.WriteLine("Su promedio es "+prom);
+        }
+    }
+}
